77c2b44d209f2c653135987d666f80ffd21b8356
Varoun P <commits@varoun.com>
2010-08-12 11:38:14 +0530

    Added names to the interp1 evaluator

--- 6.001/interp1.scm
+++ 6.001/interp1.scm
@@ -7,13 +7,21 @@
 
 ; sums
 (define (sum? exp) (tag-check exp 'plus*))
+; definitions
+(define (define? exp) (tag-check exp 'define*))
 
+;;; We need a table to store bindings. We use the hash table
+;;; ADT that comes with scheme48
+
+(define *environment* (make-table))
 
 ;;; The evaluator 
 
 (define (eval* exp)
   (cond ((number? exp) exp)
 	((sum? exp) (eval-sum exp))
+	((symbol? exp) (lookup exp))
+	((define? exp) (eval-define exp))
 	(else (error "Unknown operation in eval*"))))
 
 
@@ -28,3 +36,24 @@
 >
 |#
 
+;;; looking up the value of a symbol in the enviroment
+(define (lookup name)
+  (let ((val (table-ref *environment* name)))
+    (if val
+	val
+	(error "Unbound variable" name))))
+
+;;; definitions -- adds a binding to the environment
+(define (eval-define exp)
+  (let ((name (cadr exp))
+	(defined-to-be (caddr exp)))
+    (table-set! *environment* name (eval* defined-to-be))
+    'unspecific))
+
+#|
+> (eval* '(define* x* (plus* 4 5)))
+'unspecific
+> (eval* '(plus* x* 2))
+11
+> 
+|#
