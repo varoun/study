79e53917f9809a6d46fe42a82bb7e841e787ae5e
Varoun P <commits@varoun.com>
2010-08-19 19:00:13 +0530

    Added primitive operators to the environment. We now have apply* to deal with applications

--- 6.001/interp1.scm
+++ 6.001/interp1.scm
@@ -5,43 +5,41 @@
 (define (tag-check e sym)
   (and (pair? e) (eq? (car e) sym)))
 
-; sums
-(define (sum? exp) (tag-check exp 'plus*))
 ; definitions
 (define (define? exp) (tag-check exp 'define*))
-; predicate - greater?
-(define (greater? exp) (tag-check exp 'greater*))
 ; conditional - if?
 (define (if? exp) (tag-check exp 'if*))
+; applications
+(define (application? exp) (pair? exp))
 
 ;;; We need a table to store bindings. We use the hash table
 ;;; ADT that comes with scheme48
 
 (define *environment* (make-table))
 
+;;; primitive: ADT that stores primitive operators in the env
+(define prim-tag 'primitive)
+(define (make-primitive scheme-proc) (list prim-tag scheme-proc))
+(define (primitive? e) (tag-check e prim-tag))
+(define (get-scheme-procedure prim) (cadr prim))
+
+;;; initialise the environment
+(table-set! *environment* 'plus* (make-primitive +))
+(table-set! *environment* 'greater* (make-primitive >))
+(table-set! *environment* 'true* #t)
+
+
 ;;; The evaluator 
 
 (define (eval* exp)
   (cond ((number? exp) exp)
-	((sum? exp) (eval-sum exp))
 	((symbol? exp) (lookup exp))
 	((define? exp) (eval-define exp))
-	((greater? exp) (eval-greater exp))
 	((if? exp) (eval-if exp))
+	((application? exp) 
+	 (apply* (eval* (car exp)) (map eval* (cdr exp))))
 	(else 
 	 (error "Unknown operation in eval*"))))
-
-
-;;; Evaluating sums
-
-(define (eval-sum exp)
-  (+ (eval* (cadr exp)) (eval* (caddr exp))))
-
-#|
-> (eval* '(plus* 4 (plus* 5 6)))
-15
->
-|#
 
 ;;; looking up the value of a symbol in the enviroment
 ;;; I should really use cond here, 'if' is not primitive!
@@ -66,10 +64,6 @@
 > 
 |#
 
-;;; Evaluating the greater* predicate
-(define (eval-greater exp)
-  (> (eval* (cadr exp)) (eval* (caddr exp))))
-
 ;;; Conditionals - evaluating if* expressions
 ;;; Our primitive conditional is the 'cond' (used in eval* for ex)
 (define (eval-if exp)
@@ -89,4 +83,19 @@
 > (eval* '(if* (greater* y* 6) (plus* y* 2) 15))
 11
 > 
+|#
+
+;;; Application
+(define (apply* operator operands)
+  (if (primitive? operator)
+      (apply (get-scheme-procedure operator) operands)
+      (error "Operator not a procedure: " operator)))
+
+#|
+> (eval* '(define* z* 9))
+'unspecific
+> (eval* '(plus* z* 6))
+15
+> (eval* '(if* true* 10 15))
+10
 |#
