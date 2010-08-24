30bee99df95f65f71e96738492fca81b92c57c84
Varoun P <commits@varoun.com>
2010-08-24 12:09:42 +0530

    Added lambda expressions to the evaluator

--- 6.001/interp1.scm
+++ 6.001/interp1.scm
@@ -11,11 +11,9 @@
 (define (if? exp) (tag-check exp 'if*))
 ; applications
 (define (application? exp) (pair? exp))
+; lambda expressions
+(define (lambda? exp) (tag-check exp 'lambda*))
 
-;;; We need a table to store bindings. We use the hash table
-;;; ADT that comes with scheme48
-
-(define *environment* (make-table))
 
 ;;; primitive: ADT that stores primitive operators in the env
 (define prim-tag 'primitive)
@@ -36,6 +34,7 @@
 	((symbol? exp) (lookup exp env))
 	((define? exp) (eval-define exp env))
 	((if? exp) (eval-if exp env))
+	((lambda? exp) (eval-lambda exp env))
 	((application? exp) 
 	 (apply* (eval* (car exp) env) 
 		 (map (lambda (e) (eval* e env)) (cdr exp))))
@@ -45,16 +44,18 @@
 ;;; looking up the value of a symbol in the enviroment
 ;;; I should really use cond here, 'if' is not primitive!
 (define (lookup name env)
-  (let ((val (table-ref env name)))
-    (if val
-	val
-	(error "Unbound variable" name))))
+  (if (null? env)
+      (error "Unbound variable:" name)
+      (let ((val (table-ref (car env) name)))
+	(if val
+	    val
+	    (lookup name (cdr env))))))
 
 ;;; definitions -- adds a binding to the environment
 (define (eval-define exp env)
   (let ((name (cadr exp))
 	(defined-to-be (caddr exp)))
-    (table-set! env name (eval* defined-to-be env))
+    (table-set! (car env) name (eval* defined-to-be env))
     'unspecific))
 
 
@@ -71,10 +72,25 @@
      (else 
       (error "Predicate not a conditional: " predicate)))))
 
+;;; Evaluating lambda expressions
+;;; note: this actually does not eval anything.
+;;; we just manipulate trees.
+(define (eval-lambda exp env)
+  (let ((args (cadr exp))
+	(body (caddr exp))) ; we assume one expression in body
+    (make-compound args body env)))
+
+
 ;;; Application
 (define (apply* operator operands)
-  (if (primitive? operator)
-      (apply (get-scheme-procedure operator) operands)
+  (cond ((primitive? operator)
+	 (apply (get-scheme-procedure operator) operands))
+	((compound? operator)
+	 (eval* (body operator)
+		(extend-env-with-new-frame 
+		 (parameters operator)
+		 operands
+		 (env operator))))
       (error "Operator not a procedure: " operator)))
 
 ;> (eval* '(define* z* 9) *environment*)
@@ -86,3 +102,37 @@
 ;11
 ;> 
 
+;;; ADT that implements the 'double-bubble'
+(define compound-tag 'compound)
+(define (make-compound parameters body env)
+  (list compound-tag parameters body env))
+(define (compound? exp) (tag-check exp compound-tag))
+(define (parameters compound) (cadr compound))
+(define (body compound) (caddr compound))
+(define (env compound) (cadddr compound))
+
+;;; The environment
+(define (extend-env-with-new-frame names values env)
+  (let ((new-frame (make-table))) ; make-table: hashtables in s48 
+    (make-bindings! names values new-frame)
+    (cons new-frame env)))
+
+(define (make-bindings! names values frame)
+  (for-each 
+   (lambda (name value)
+     (table-set! frame name value))
+   names values))
+
+;;; Initialise the global environment
+(define *GE*
+  (extend-env-with-new-frame
+   (list 'plus* 'greater*)
+   (list (make-primitive +) (make-primitive >))
+   '()))
+
+
+;> (eval* '(define* twice* (lambda* (x*) (plus* x* x*))) *GE*)
+;'unspecific
+;> (eval* '(twice* 4) *GE*)
+;8
+;> 
